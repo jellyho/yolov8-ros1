88f6224e461e0d32733b833e0d4385fe1e2ef64f
jellyho <jellyho@naver.com>
2024-03-27 17:53:27 +0900

    weight update & publish json

--- scripts/yolo.py
+++ scripts/yolo.py
@@ -2,11 +2,12 @@
 
 import rospy, rospkg
 import cv2, torch
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
+rospack = rospkg.RosPack()
 
 latest_image = None
 
@@ -22,12 +23,20 @@ def image_subscriber(image_msg):
         rospy.logerr(e)
 
 def image_subscriber_node():
-    rospy.init_node('yolo_node', anonymous=True)
-    
+    rospy.init_node('yolov8_node', anonymous=True)
+    weights = rospy.get_param('~weights')
+    if weights == "None":
+        rospy.loginfo("USE Default pretrained weights - yolov8s.pt")
+        weightPath = 'yolov8s.pt'
+    else:
+        weightPath = rospack.get_path('yolov8') + f'/src/{weights}'
+    model = YOLO(weightPath)
+
     topic = rospy.get_param('~image')
     rospy.Subscriber(topic, Image, image_subscriber)
     verbose = rospy.get_param('~verbose')
     publish = rospy.get_param('~publish')
+    json_publisher = rospy.Publisher('/yolo_results', String, queue_size=1)
 
     if publish:
         publisher = rospy.Publisher('/yolo_image', Image, queue_size=1)
@@ -39,13 +48,18 @@ def image_subscriber_node():
             
             plotted = results.plot()
 
+            try:
+                json_publisher.publish(String(results.tojson()))
+            except:
+                pass
+
             if verbose:
                 cv2.imshow("Received Image", plotted)
                 cv2.waitKey(1)
 
-            # if publish:
-            #     bridge = CvBridge()
-            #     publisher.publish(bridge.cv2_to_imgmsg(frame, encoding="bgr8"))
+            if publish:
+                bridge = CvBridge()
+                publisher.publish(bridge.cv2_to_imgmsg(plotted, encoding="bgr8"))
 
 if __name__ == '__main__':
     try:
